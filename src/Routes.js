e624acdca05747d531434ea16657e9efd296f217
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2020-12-29 11:43:51 -0300

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React, { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { Layout } from './layout-blueprints';
+
+const LandingPage = lazy(() => import('./example-pages/LandingPage'));
+
+const Routes = () => {
+    return (
+        <AnimatePresence>
+            <Suspense
+                fallback={
+                    <div className="d-flex align-items-center vh-100 justify-content-center text-center font-weight-bold font-size-lg py-3">
+                        <div className="w-50 mx-auto">Cargando...</div>
+                    </div>
+                }>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={LandingPage}></Route>
+                    </Switch>
+                </Layout>
+            </Suspense>
+        </AnimatePresence>
+    );
+};
+
+export default Routes;
---
+++ src/layout-components/Header/index.js
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Container
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+            mouseOverTwitter: false,
+            mouseOverFacebook: false,
+            mouseOverInstagram: false
+        };
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
+
+    goToSection = section => {
+        console.log(section);
+    };
+
+    handleMouseOver = boton => {
+        switch (boton) {
+            case 'mouse on facebook':
+                this.setState({
+                    mouseOverFacebook: true
+                });
+                break;
+            case 'mouse on twitter':
+                this.setState({
+                    mouseOverTwitter: true
+                });
+                break;
+            case 'mouse on instagram':
+                this.setState({
+                    mouseOverInstagram: true
+                });
+                break;
+
+            default:
+                console.log(boton);
+        }
+    };
+
+    handleMouseOut = boton => {
+        switch (boton) {
+            case 'mouse out of facebook':
+                this.setState({
+                    mouseOverFacebook: false
+                });
+                break;
+            case 'mouse out of twitter':
+                this.setState({
+                    mouseOverTwitter: false
+                });
+                break;
+            case 'mouse out of instagram':
+                this.setState({
+                    mouseOverInstagram: false
+                });
+                break;
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <Navbar
+                    color="primary"
+                    fixed="top"
+                    light
+                    expand="md"
+                    className="bg-primary text-white">
+                    <Container>
+                        <NavbarBrand href="/" className="text-white">
+                            Agromix SA Buenos Aires
+                        </NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <a href="#" className="nav-link text-white">
+                                        Quiénes Somos
+                                    </a>
+                                </NavItem>
+                                <NavItem>
+                                    <a href="#" className="nav-link text-white">
+                                        Nuestros Productos
+                                    </a>
+                                </NavItem>
+                                <NavItem>
+                                    <a href="#" className="nav-link text-white">
+                                        Contacto
+                                    </a>
+                                </NavItem>
+                                <UncontrolledDropdown nav inNavbar>
+                                    <DropdownToggle nav caret className="text-white">
+                                        Nuestras Redes Sociales
+                                    </DropdownToggle>
+                                    <DropdownMenu right>
+                                        <DropdownItem
+                                            className={
+                                                this.state.mouseOverFacebook ? 'text-first' : ''
+                                            }
+                                            onMouseOver={() =>
+                                                this.handleMouseOver('mouse on facebook')
+                                            }
+                                            onMouseOut={() =>
+                                                this.handleMouseOut('mouse out of facebook')
+                                            }>
+                                            <FontAwesomeIcon
+                                                icon={faFacebook}
+                                                className="font-size-xxl mr-3"
+                                            />
+                                            Facebook
+                                        </DropdownItem>
+                                        <DropdownItem divider />
+                                        <DropdownItem
+                                            className={
+                                                this.state.mouseOverTwitter ? 'text-first' : ''
+                                            }
+                                            onMouseOver={() =>
+                                                this.handleMouseOver('mouse on twitter')
+                                            }
+                                            onMouseOut={() =>
+                                                this.handleMouseOut('mouse out of twitter')
+                                            }>
+                                            <FontAwesomeIcon
+                                                icon={faTwitter}
+                                                className="font-size-xxl mr-3"
+                                            />
+                                            Twitter
+                                        </DropdownItem>
+                                        <DropdownItem divider />
+                                        <DropdownItem
+                                            className={
+                                                this.state.mouseOverInstagram ? 'text-first' : ''
+                                            }
+                                            onMouseOver={() =>
+                                                this.handleMouseOver('mouse on instagram')
+                                            }
+                                            onMouseOut={() =>
+                                                this.handleMouseOut('mouse out of instagram')
+                                            }>
+                                            <FontAwesomeIcon
+                                                icon={faInstagram}
+                                                className="font-size-xxl mr-3"
+                                            />
+                                            Instagram
+                                        </DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default Header;
