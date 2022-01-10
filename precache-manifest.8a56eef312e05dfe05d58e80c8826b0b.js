c55c891e0ea27ad9294b40ff03dbb04fb5fda82f
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-10 15:32:41 -0300

    Updates

---
+++ precache-manifest.8a56eef312e05dfe05d58e80c8826b0b.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09053df8bc2d38f628797f4e5a8d14c",
+    "url": "/agromix/index.html"
+  },
+  {
+    "revision": "c715a2a99aa681e6e0e7",
+    "url": "/agromix/static/css/main.0593520c.chunk.css"
+  },
+  {
+    "revision": "766f97c89514419e5351",
+    "url": "/agromix/static/js/2.3c581cac.chunk.js"
+  },
+  {
+    "revision": "b4a638b455b533c8e657f6a60c2ccd14",
+    "url": "/agromix/static/js/2.3c581cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8d2452ff8ca767787c",
+    "url": "/agromix/static/js/3.cccca8ab.chunk.js"
+  },
+  {
+    "revision": "e0fbe745b5d5ca1e562c",
+    "url": "/agromix/static/js/4.e65de647.chunk.js"
+  },
+  {
+    "revision": "c715a2a99aa681e6e0e7",
+    "url": "/agromix/static/js/main.ec6d78cd.chunk.js"
+  },
+  {
+    "revision": "66544945cc88acbd225f",
+    "url": "/agromix/static/js/runtime-main.5a63e47e.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/agromix/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/agromix/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/agromix/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/agromix/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "59fbdece2a87b64eb875a6557c6adfd3",
+    "url": "/agromix/static/media/agromix.59fbdece.png"
+  },
+  {
+    "revision": "ef96b0fd01d526cad7e40ddb5dd2a2c1",
+    "url": "/agromix/static/media/bg1.ef96b0fd.jpg"
+  },
+  {
+    "revision": "456b47bce336ad14b0da2b679a1ea9e7",
+    "url": "/agromix/static/media/bg2.456b47bc.jpg"
+  },
+  {
+    "revision": "bade0f805119c4e72979c6455fec5e1f",
+    "url": "/agromix/static/media/bg3.bade0f80.jpg"
+  },
+  {
+    "revision": "ae3c874d0f8ad41aee2a32dae5fab77b",
+    "url": "/agromix/static/media/bg4.ae3c874d.jpg"
+  },
+  {
+    "revision": "d67305229d9cbfe677b1498d5941d5e0",
+    "url": "/agromix/static/media/bg5.d6730522.jpg"
+  },
+  {
+    "revision": "66ac1a1af07cd368c5c27bb0a2beaa05",
+    "url": "/agromix/static/media/bioinnovo.66ac1a1a.png"
+  },
+  {
+    "revision": "5c510557b7ff4c49350f18c9668af652",
+    "url": "/agromix/static/media/biscayart.5c510557.png"
+  },
+  {
+    "revision": "28fec801e685015328070bb3d4f66703",
+    "url": "/agromix/static/media/catalogo.28fec801.jpeg"
+  },
+  {
+    "revision": "4867d1d905e8cd1fdb3b619ba2122ae5",
+    "url": "/agromix/static/media/durflex.4867d1d9.png"
+  },
+  {
+    "revision": "fca87cc69392c05e29e269c194df9a86",
+    "url": "/agromix/static/media/folleto.fca87cc6.jpeg"
+  },
+  {
+    "revision": "61cf7d928ba82d3c3458e531c9140566",
+    "url": "/agromix/static/media/foto-1.61cf7d92.jpeg"
+  },
+  {
+    "revision": "10c593abc9d7877d01aeb34fd472e96b",
+    "url": "/agromix/static/media/foto-10.10c593ab.jpeg"
+  },
+  {
+    "revision": "9abbf409fe564b9997b29bc1b2d4b096",
+    "url": "/agromix/static/media/foto-4.9abbf409.jpeg"
+  },
+  {
+    "revision": "ae7c37b91de2eeef95f424d311b9875b",
+    "url": "/agromix/static/media/foto-5.ae7c37b9.jpeg"
+  },
+  {
+    "revision": "6a0d90a1c09ab0e2b749931431d467f2",
+    "url": "/agromix/static/media/foto-6.6a0d90a1.jpeg"
+  },
+  {
+    "revision": "edb7241520436a957106658b848b9162",
+    "url": "/agromix/static/media/foto-7.edb72415.jpeg"
+  },
+  {
+    "revision": "8a74a00ae7c008dbdc099e6117367488",
+    "url": "/agromix/static/media/foto-8.8a74a00a.jpeg"
+  },
+  {
+    "revision": "f86aab79eb9b03a3569ae8f3b67f1f06",
+    "url": "/agromix/static/media/foto-9.f86aab79.jpeg"
+  },
+  {
+    "revision": "11c77b9ddb881fa682aea383915996ae",
+    "url": "/agromix/static/media/royalcanin.11c77b9d.jpg"
+  },
+  {
+    "revision": "3c2a86d43c0c53992f79d662eee41ec1",
+    "url": "/agromix/static/media/syntek.3c2a86d4.png"
+  },
+  {
+    "revision": "422a12527b5f902852fc30e77912d140",
+    "url": "/agromix/static/media/wallpaper.422a1252.jpeg"
+  },
+  {
+    "revision": "d40d9bbf660e6be1c3cf4752e07dc01d",
+    "url": "/agromix/static/media/zoovet.d40d9bbf.png"
+  }
+]);
